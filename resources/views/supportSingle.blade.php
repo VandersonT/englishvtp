f05abca962edeff093da091a9fe1ca28ce4197e4
VandersonThiago <vandersontpaulo@gmail.com>
2021-08-31 20:08:06 -0300

    Criado o layout dos comentarios de um suporte

--- resources/views/supportSingle.blade.php
+++ resources/views/supportSingle.blade.php
@@ -8,12 +8,32 @@
 
 <!--Links-->
 @section('links')
-    
+    <link rel="stylesheet" href="<?=$base_url?>/assets/css/supportSingle.css" />
 @endsection
 
 <!--Content-->
 @section('content')
-    
+    <h1 class="title"><?=$supportInfo['title'];?></h1>
+
+    <section class="box-comment">
+        <div class="commentSingle">
+            <div class="box-info">
+                <img src="<?=$base_url?>/media/avatars/<?=$user['photo'];?>" />
+                <div class="infouser">
+                    <p><?=$user['name'];?></p>
+                    <span><?=date('d/m/Y H:i', $supportInfo['date']);?></span>
+                </div>
+            </div>
+            <p>
+                <?=$supportInfo['content'];?>
+            </p>
+        </div>
+    </section>
+
+    <section class="box-writeAReply">
+
+    </section>
+
 @endsection
 
 <!--Scripts-->
